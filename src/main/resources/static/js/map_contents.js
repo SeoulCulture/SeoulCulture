1d40b127a1c94daadc3579cfcde4e06a07162deb
skj4244 <skj4244@naver.com>
2024-05-06 23:58:27 +0900

    feat: detail url 유효성 검사

--- src/main/resources/static/js/map_contents.js
+++ src/main/resources/static/js/map_contents.js
@@ -25,6 +25,7 @@ function relocationMarkers(){
 
 
 function setInformation(marker) {
+    console.log(marker)
     $(".place-category").text(marker.category);
     $(".place-title").text(marker.title);
     const placeImg = document.querySelectorAll('.place-img img');
@@ -35,10 +36,27 @@ function setInformation(marker) {
     $(".place-price").html("가격: " + marker.price);
     const placeDetail = document.querySelectorAll('.place-detail');
     placeDetail.forEach(function(button) {
-        button.setAttribute('onclick', `window.open('${marker.detailUrl}')`);
+        button.setAttribute('onclick', `openDetailUrl('${marker.detailUrl}')`);
     });
-
 }
+
+function openDetailUrl(url){
+    if(!checkUrlValidate(url)){
+        alert('홈페이지 주소가 존재하지 않습니다. 이용에 불편을 드려 죄송합니다.')
+    }else{
+        window.open(url)
+    }
+}
+
+function checkUrlValidate(url){
+    try {
+        new URL(url);
+        return true;
+    } catch (err) {
+        return false;
+    }
+}
+
 
 window.addEventListener('resize', function() {
     // relocationMarkers();
