660377baea37356344ae13695c0f53e1efbbeeaa
yyeennyy <kye3314@gmail.com>
2024-05-11 17:28:59 +0900

    design: paragraph event

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,6 +1,8 @@
 let spotDtos;
 
 document.addEventListener("DOMContentLoaded", function() {
+  showParagraph();
+
   const spotUrl = '/spots';
   fetch(spotUrl)
       .then(response => {
@@ -49,4 +51,22 @@ function toggleLocationBox(checkbox){
   }else {
     locationInput.focus();
   }
+}
+
+
+function showParagraph() {
+  var paragraphs = ["paragraph1", "paragraph2", "paragraph3"];
+  var index = 0;
+
+  document.getElementById(paragraphs[index]).style.opacity = "1";
+
+  var interval = setInterval(function() {
+    // 다음 단락 표시
+    index++;
+    if (index >= paragraphs.length) {
+      clearInterval(interval); // 마지막 단락까지 표시되면 인터벌 정지
+    } else {
+      document.getElementById(paragraphs[index]).style.opacity = "1";
+    }
+  }, 1000);
 }
