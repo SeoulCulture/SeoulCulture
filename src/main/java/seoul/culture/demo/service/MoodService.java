afa44967566e2112b4665c5b9b2b449f409f530f
yyeennyy <kye3314@gmail.com>
2024-05-11 20:12:44 +0900

    fix: read only 해제

--- src/main/java/seoul/culture/demo/service/MoodService.java
+++ src/main/java/seoul/culture/demo/service/MoodService.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
+@Transactional
 public class MoodService {
 
     private final MoodRepository moodRepository;
